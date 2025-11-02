c9eb4f06619103e53296fd1fd32e112cf1b8c1b5
Julliane J. Tampus <jull@Jullianes-MacBook-Air.local>
2025-11-02 23:54:58 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='header'>
+        <h1>Admin Pannel</h1>
+      </div>
+      <div className='container'>
+        <div className='form'>
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <button>Submit</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
